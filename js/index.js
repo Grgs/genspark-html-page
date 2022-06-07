e520ae42fb9266c1937332e6370733aed88f61ad
Michael Guirguis <guirguis@mguirguis.com>
2022-06-07 08:55:27 -0400

    added required fields and changed showing fields

--- js/index.js
+++ js/index.js
@@ -1,21 +1,56 @@
 
 function showtext() {
+  var showElement = document.getElementById("form-result");
+  var inputElements = document.querySelectorAll("input");
+  for (var i = 0; i < inputElements.length; i++) {
+    showElement.innerHTML += inputElements[i].id + ": " + inputElements[i].value + "<br>";
+  }
+    // if (inputElements[i].value == "") {
+    //   showElement.innerHTML = "Please fill in all fields";
+    //   return;
+    // }
 
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var fullName = firstName + " " + lastName;
-  var email = document.getElementById("email").value;
-  var phone = document.getElementById("phone").value;
-  var address = document.getElementById("address").value;
-  var city = document.getElementById("city").value;
-  var state = document.getElementById("state").value;
-  var zip = document.getElementById("zip").value;
-  var birthday = document.getElementById("birthday").value;
-
-  document.getElementById("form-result").innerHTML = "<div class='alert alert-info' role='alert'>" +
-    fullName + "<br>" + email + "<br>" + phone + "<br>" + address + "<br>" + city + "<br>" + state + "<br>" + zip + "<br>" + birthday + "</div>";
 }
+
 
 function hidetext() {
   document.getElementById("form-result").innerHTML = "";
 }
+
+(() => {
+  'use strict';
+
+
+})();
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+
+})();
+
+
+// function validatePassword() {
+//   var password = document.getElementById("password").value;
+//   var confirmPassword = document.getElementById("confirmPassword").value;
+
+//   if (password != confirmPassword) {
+//     document.getElementById("confirmPassword").setCustomValidity("Passwords Don't Match");
+//   } else {
+//     document.getElementById("confirmPassword").setCustomValidity('');
+//   }
+// }
